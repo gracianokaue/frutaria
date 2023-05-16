deb015d218e90984e0a063ba1656cb04d8646cb0
Kaue Graciano <kauegraciano93@hotmail.com>
2023-05-15 22:39:42 -0300

    Criado o componente de formulário de cadastro com algumas configurações iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormUsuarioComponent } from './components/form-usuario/form-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormUsuarioComponent
   ],
   imports: [
     BrowserModule,
